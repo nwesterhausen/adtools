45071d21cf0810a163757990314570416e2412a2
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-08-23 16:30:01 -0400

    Fix window size

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 844,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
