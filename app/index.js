53f6a407ac658a87d460228d2f5fcb0212dc977e
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-09-06 10:49:32 -0400

    "Properly" importing html templates

--- app/index.js
+++ app/index.js
@@ -1,6 +1,25 @@
 // Require Dependencies
-const $ = require('jquery');
-const bs = require('bootstrap');
-const powershell = require('node-powershell');
 const path = require('path');
 const { remote } = require('electron');
+
+function getTemplate(filepath) {
+  return fetch(filepath)
+    .then(response => {
+      return response.text();
+    })
+    .then(txt => {
+      let html = new DOMParser().parseFromString(txt, 'text/html');
+      return html.querySelector('template').content;
+    });
+}
+
+// Load New User Page
+getTemplate(path.join(__dirname, 'templates/newUser.html')).then(val => {
+  console.log(val);
+  $('#newuser').html(val);
+});
+// Load edit User Page
+getTemplate(path.join(__dirname, 'templates/userDetails.html')).then(val => {
+  console.log(val);
+  $('#user').html(val);
+});
